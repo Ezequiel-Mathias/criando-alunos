82716a5c969983fe3e107f248c3195c866a25a80
Ezequiel Mathias <ezequielmathias32@gmail>
2021-11-11 16:57:59 -0300

    aula dia 11/11

--- src/br/senai/sp/jandira/ui/FrameCadastroAlunos.java
+++ src/br/senai/sp/jandira/ui/FrameCadastroAlunos.java
@@ -12,17 +12,26 @@ import javax.swing.JButton;
 import javax.swing.JSlider;
 import java.awt.Panel;
 import java.awt.ScrollPane;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JList;
 import javax.swing.JComboBox;
 import javax.swing.JCheckBox;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.DefaultListModel;
+
+import br.senai.sp.jandira.model.Aluno;
 import br.senai.sp.jandira.model.Periodo;
+import br.senai.sp.jandira.repository.AlunoRepository;
 
 public class FrameCadastroAlunos extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
+	
+	private int posicao = 0;
 
 	/**
 	 * Launch the application.
@@ -44,6 +53,9 @@ public class FrameCadastroAlunos extends JFrame {
 	 * Create the frame.
 	 */
 	public FrameCadastroAlunos() {
+		
+		String dias [] = {"segunda", "terça", "quarta", "quinta"};
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -52,7 +64,7 @@ public class FrameCadastroAlunos extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("matricula");
-		lblNewLabel.setBounds(20, 35, 47, 17);
+		lblNewLabel.setBounds(10, 35, 57, 17);
 		contentPane.add(lblNewLabel);
 		
 		textField = new JTextField();
@@ -61,7 +73,7 @@ public class FrameCadastroAlunos extends JFrame {
 		textField.setColumns(10);
 		
 		JLabel lblName = new JLabel("name");
-		lblName.setBounds(20, 64, 41, 14);
+		lblName.setBounds(10, 64, 51, 14);
 		contentPane.add(lblName);
 		
 		textField_1 = new JTextField();
@@ -70,15 +82,35 @@ public class FrameCadastroAlunos extends JFrame {
 		contentPane.add(textField_1);
 		
 		JLabel lblPeriodo = new JLabel("periodo");
-		lblPeriodo.setBounds(20, 89, 41, 14);
+		lblPeriodo.setBounds(10, 89, 51, 14);
 		contentPane.add(lblPeriodo);
 		
 		JButton btnNewButton = new JButton("Save");
 		btnNewButton.setBounds(64, 128, 89, 23);
 		contentPane.add(btnNewButton);
 		
+		AlunoRepository turmas = new AlunoRepository(3);
+		
+		btnNewButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				Aluno aluno = new Aluno();
+				aluno.setMatricula(lblNewLabel.getText());
+				aluno.setNome(lblName.getText());
+				turmas.gravar(aluno, posicao);
+				
+				posicao++;
+				
+			}
+		});
+		
+		
+
+		
 		JLabel lblListaDeAlunos = new JLabel("Lista De Alunos :");
-		lblListaDeAlunos.setBounds(251, 36, 86, 14);
+		lblListaDeAlunos.setBounds(229, 36, 108, 14);
 		contentPane.add(lblListaDeAlunos);
 		
 		ScrollPane scrollPane = new ScrollPane();
@@ -86,13 +118,41 @@ public class FrameCadastroAlunos extends JFrame {
 		contentPane.add(scrollPane);
 		
 		JList list = new JList();
+		//adicionar o nome do aluno na lista
+		DefaultListModel<String> listModel = new DefaultListModel<String>();
 		list.setBounds(229, 63, 195, 187);
 		contentPane.add(list);
 		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(Periodo.values()));
+		//para colocar "lista" no combobox usa-se as duas linhas abaixo
+		DefaultComboBoxModel<String> modelperiodo = new DefaultComboBoxModel<String>(dias);
+		//usei o for para não ter que fazer coisas repetitivas !!
+		for(Periodo P : Periodo.values()) {
+			modelperiodo.addElement(P.getDescricao());
+		}
+		comboBox.setModel(modelperiodo);
 		comboBox.setBounds(67, 85, 86, 22);
 		contentPane.add(comboBox);
+		
+		JButton btnNewButton_1 = new JButton("Exibir Alunos");
+		btnNewButton_1.setBounds(64, 174, 89, 23);
+		contentPane.add(btnNewButton_1);
 		contentPane.setVisible(true);
+		
+		btnNewButton_1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				for (Aluno aluno : turmas.listarTodos()) {
+					System.out.println(aluno.getMatricula());
+					System.out.println(aluno.getNome());
+					System.out.println(aluno.getPeriodo());
+				}
+				
+				
+			}
+		});
+		
 	}
 }
